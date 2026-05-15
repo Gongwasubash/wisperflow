ce6797b437877c360fd09c794572a2a3b11e8f69
Gongwasubash <subashgongwanepal@gmail.com>
2026-05-15 09:19:12 +0545

    Use regular Netlify function instead of edge function

--- netlify/functions/env.js
+++ netlify/functions/env.js
@@ -1,8 +1,10 @@
-export default function handler(req) {
-  return new Response(
-    `window.WF_GROQ_API_KEY = '${process.env.GROQ_API_KEY}';`,
-    {
-      headers: { 'Content-Type': 'application/javascript' }
-    }
-  );
-}
+exports.handler = async function(event, context) {
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/javascript',
+      'Cache-Control': 'no-cache, no-store, must-revalidate'
+    },
+    body: `window.WF_GROQ_API_KEY = '${process.env.GROQ_API_KEY || ''}';`
+  };
+};
